829717dde2f95d18b84c9f4210c3f49cde3ab6ae
jh-jin <devjin0122@gmail.com>
2022-03-10 21:05:44 +0900

    Feat: Container 컴포넌트 추가

---
+++ src/Screens/CreateBook.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Container } from "../components/Container";
+
+function CreateBook() {
+  return (
+    <Container>
+      <form>{/* searchBar */}</form>
+      <input></input>
+    </Container>
+  );
+}
+
+export default CreateBook;
