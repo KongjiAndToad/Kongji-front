82a449b93b3d2b9e025747cbbf41988e43a3621e
jh-jin <devjin0122@gmail.com>
2022-03-21 14:04:28 +0900

    Feat: BookList 컴포넌트 구현

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,7 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import styled from "styled-components";
+import "@fontsource/noticia-text";
+import { useHistory } from "react-router-dom";
 
-function BookList() {
-  return <></>;
+function BookList({ book }) {
+  //history
+  const history = useHistory();
+
+  //BookList 순회하면서 출력하는 건 DrawMain에서
+  const gotoPage = () => {
+    history.push(`/createbook/${book.create}`);
+  };
+
+  return (
+    <BookListWrap>
+      <div onClick={gotoPage} className="list-container">
+        <div className="list-title">{book.title}</div>
+      </div>
+    </BookListWrap>
+  );
 }
 
 export default BookList;
+
+const BookListWrap = styled.div`
+  margin-bottom: 24px;
+  .list-container {
+    width: 583px;
+    height: 111px;
+    background: #ffffff;
+    border-radius: 20px;
+  }
+  .list-title {
+    padding-top: 21px;
+    padding-left: 22px;
+    font-family: Noticia Text;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 24px;
+    line-height: 33px;
+    letter-spacing: 0.03em;
+    color: #0a0909;
+  }
+`;
